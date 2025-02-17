355160bd242ac251690c6202a33238328bc33785
wensm <wensm@DESKTOP-TOQOA6R>
2025-02-17 01:24:44 -0400

    Add project files.

---
+++ Tetris/GameState.cs
@@ -0,0 +1,18 @@
+﻿namespace Tetris
+{
+    public class GameState
+    {
+        private Block currentBlock;
+
+        public Block currentBlock
+        {
+            get => currentBlock;
+            private set
+            {
+                currentBlock = value;
+                currentBlock.reset();
+            }
+        }
+
+    }
+}
